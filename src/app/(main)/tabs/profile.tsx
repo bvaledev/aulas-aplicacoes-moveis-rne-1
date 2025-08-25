6d309bd117ea4eb5c303f33c123a97bf07ba4bbe
Brendo Vale <brendo.vale02@gmail.com>
2025-08-24 22:22:01 -0400

    implementação tabs route

---
+++ src/app/(main)/tabs/profile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tela Perfil</Text>
+      <Text style={{ textAlign: 'center' }}>Esta é a página de perfil!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center', padding: 20 },
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 10 },
+});
